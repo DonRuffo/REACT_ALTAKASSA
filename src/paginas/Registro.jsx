193766409661a51f06565eab31135e7b2baff7ae
FAMILIA <dennisdiaz407@gmail.com>
2025-05-15 15:45:25 -0500

    probando2

--- src/paginas/Registro.jsx
+++ src/paginas/Registro.jsx
@@ -107,11 +107,7 @@ const Registro = () => {
                                         <option value="Galápagos">Galápagos</option>
                                     </select>
                                 </div>
-                                <div className="mb-3 grid grid-cols-2">
-                                    <div className="mx-1">
-                                        <label htmlFor="telefono" className="mb-2 block text-md font-semibold dark: text-purple-500">Teléfono</label>
-                                        <input type="text" name='telefono' id="telefono" placeholder="Ingresa tu teléfono" onChange={HandleChange} value={form.telefono || ""} className=" block w-full dark:bg-transparent dark:text-white rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500" />
-                                    </div>
+                                <div className="mb-3">
                                     <div className="mx-1">
                                         <label htmlFor="email" className="mb-2 block text-md font-semibold dark: text-purple-500">Correo electrónico</label>
                                         <input type="email" name='email' id="email" placeholder="Ingresa tu correo" onChange={HandleChange} value={form.email || ""} className=" block w-full dark:bg-transparent dark:text-white rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500" />
