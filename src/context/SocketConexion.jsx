8ef006c237e7936683042598d5abf1bac71c2ce0
FAMILIA <dennisdiaz407@gmail.com>
2025-07-07 09:20:12 -0500

    Optimizando

--- src/context/SocketConexion.jsx
+++ src/context/SocketConexion.jsx
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client'
 
 const socket = io(import.meta.env.VITE_BACK_URI, {
+    transports: ["websocket"],
     autoConnect: true,
     reconnection: true,
     reconnectionAttempts: 6,
